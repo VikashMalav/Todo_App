68e70794f596fd063bdbca19d25dfe5c44062ba3
VikashMalav <malavvicky99@gmail.com>
2023-02-19 22:51:59 +0530

    index added

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TodoList from './Todo/Todo2';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+ <TodoList/>
+
+);
+
